2bc9ff4ea7c15e5ed589eed2d4283e95f87da2f5
Sifan Jiang <sifanj@kth.se>
2018-10-13 17:35:40 +0200

    Change generateBinarySignal.m

--- getStationaryAverages.m
+++ getStationaryAverages.m
@@ -1,3 +1,6 @@
 function bar_y = getStationaryAverages(y_step, Nwr, tail)
-
+    wrLen = length(y_step)/Nwr;
+    for i=1:Nwr
+        bar_y(i) = getAverage(y_step((i-1)*wrLen+1:(i)*wrLen),tail);
+    end
 end
